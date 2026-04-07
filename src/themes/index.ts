bd50eb8bc872c0d5d33058ed3a360bbef727fd3b
apxllo1 <bravounlimitaura@gmail.com>
2026-04-07 05:26:32 -0700

    Replace 'sorbet' with 'crimson' in theme list

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -3,10 +3,10 @@ import { frostedGlass } from "themes/frosted-glass";
 import { highContrast } from "themes/high-contrast";
 import { lightTheme } from "themes/light-theme";
 import { obsidian } from "themes/obsidian";
-import { sorbet } from "themes/sorbet";
+import { crimson } from "themes/crimson";
 import { Theme } from "themes/theme.interface";
 
-const themes: Theme[] = [sorbet, darkTheme, lightTheme, frostedGlass, obsidian, highContrast];
+const themes: Theme[] = [crimson, darkTheme, lightTheme, frostedGlass, obsidian, highContrast];
 
 export function getThemes() {
 	return themes;
