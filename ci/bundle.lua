3b271a8603af28ec88f5b66c78109b4d06a51290
apxllo1 <bravounlimitaura@gmail.com>
2026-04-07 00:48:39 -0700

    Change ROJO_INPUT from 'Orca.rbxm' to 'Havoc.rbxm'

--- ci/bundle.lua
+++ ci/bundle.lua
@@ -15,7 +15,7 @@ local DEBUG_MODE = getFlag("debug")
 local VERBOSE = getFlag("verbose")
 local MINIFY = getFlag("minify")
 
-local ROJO_INPUT = "Orca.rbxm"
+local ROJO_INPUT = "Havoc.rbxm"
 local RUNTIME_FILE = "ci/runtime.lua"
 local BUNDLE_TEMP = "ci/bundle.tmp"
 
@@ -23,28 +23,16 @@ local BUNDLE_TEMP = "ci/bundle.tmp"
 ---@param source string
 ---@return string
 local function transformInput(source)
-	-- Capture (var) (+-/*%^..)= (value)
-	-- Substitute %1 = %1 %2 %3
 	source = string.gsub(source, "([%w_]+)%s*([%+%-%*/%%^%.]%.?)=%s*", "%1 = %1 %2")
-
-	-- Capture whole word 'continue'
-	-- Substitute __CONTINUE__()
 	source = string.gsub(source, "(%s+)continue(%s+)", "%1__CONTINUE__()%2")
-
 	return source
 end
 
 ---@param source string
 ---@return string
 local function transformOutput(source)
-	-- Substitute ...: with (...):
-	-- For a luamin bug caused by calling varargs
 	source = string.gsub(source, "%.%.%.:", "(...):")
-
-	-- Capture __CONTINUE__()
-	-- Substitute continue
 	source = string.gsub(source, "__CONTINUE__%(%)", "continue;")
-
 	return source
 end
 
@@ -52,12 +40,9 @@ end
 ---@return string
 local function minify(source)
 	remodel.writeFile(BUNDLE_TEMP, transformInput(source))
-
 	os.execute("node ci/minify.js")
 	local output = remodel.readFile(BUNDLE_TEMP)
-
 	os.remove(BUNDLE_TEMP)
-
 	return transformOutput(output)
 end
 
@@ -127,25 +112,21 @@ local function main()
 	local output = {}
 	local model = remodel.readModelFile(ROJO_INPUT)[1]
 
-	-- Add instances
 	writeInstanceTree(model, output)
 
-	-- Minify current output
 	if MINIFY then
 		output = { minify(table.concat(output, "\n")) }
 	end
 
-	-- Core runtime
 	local runtime = string.gsub(remodel.readFile(RUNTIME_FILE), "__VERSION__", string.format("%q", VERSION))
 	table.insert(output, 1, runtime)
 	table.insert(output, "init()")
 
 	if VERBOSE then
 		table.insert(output, 2, "local START_TIME = os.clock()")
-		table.insert(output, "print(\"[CI " .. VERSION .. "] Orca run in \" .. (os.clock() - START_TIME) * 1000 .. \" ms\")")
+		table.insert(output, "print(\"[CI \" .. VERSION .. \"] Havoc run in \" .. (os.clock() - START_TIME) * 1000 .. \" ms\")")
 	end
 
-	-- Write to file
 	remodel.createDirAll(string.match(OUTPUT_PATH, "^(.*)[/\\]"))
 	remodel.writeFile(OUTPUT_PATH, table.concat(output, "\n\n"))
 
