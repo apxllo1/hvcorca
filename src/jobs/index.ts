032be8aaef3e220403b94e59468bd5b687a702a6
apxllo1 <bravounlimitaura@gmail.com>
2026-04-26 18:38:43 -0500

    Import new Facebang logic in index.ts

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -1,1 +1,18 @@
 export { setStore } from "./helpers/job-store";
+
+// This tells the script to load your new Facebang logic
+import "./facebang.client";
+
+// These are your existing features
+import "./acrylic.client";
+import "./freecam.client";
+import "./server.client";
+import "./character/flight.client";
+import "./character/ghost.client";
+import "./character/godmode.client";
+import "./character/humanoid.client";
+import "./character/refresh.client";
+import "./players/hide.client";
+import "./players/kill.client";
+import "./players/spectate.client";
+import "./players/teleport.client";
