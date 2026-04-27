0762b898c1111e7ba7415d86e1c818af57d44c2c
apxllo1 <bravounlimitaura@gmail.com>
2026-04-26 22:54:21 -0500

    Add Misc page routing to Pages component
    Added routing for the Misc page in the Pages component.

--- src/views/Pages/Pages.tsx
+++ src/views/Pages/Pages.tsx
@@ -3,21 +3,27 @@ import { hooked } from "@rbxts/roact-hooked";
 import { useDelayedUpdate } from "hooks/common/use-delayed-update";
 import { useCurrentPage } from "hooks/use-current-page";
 import { DashboardPage } from "store/models/dashboard.model";
+
 import Apps from "./Apps";
 import Home from "./Home";
 import Options from "./Options";
 import Scripts from "./Scripts";
-import Misc from "./Misc/Misc"; // ADD THIS
+import Misc from "./Misc/Misc"; // The file we just created
 
 function Pages() {
 	const currentPage = useCurrentPage();
+
+	// This helps prevent lag when switching to the heavy Scripts list
 	const isScriptsVisible = useDelayedUpdate(currentPage === DashboardPage.Scripts, 2000, (isVisible) => isVisible);
+
 	return (
 		<>
 			{currentPage === DashboardPage.Home && <Home Key="home" />}
 			{currentPage === DashboardPage.Apps && <Apps Key="apps" />}
 			{isScriptsVisible && <Scripts Key="scripts" />}
 			{currentPage === DashboardPage.Options && <Options Key="options" />}
+			
+			{/* Added the Misc Page routing here */}
 			{currentPage === DashboardPage.Misc && <Misc Key="misc" />}
 		</>
 	);
