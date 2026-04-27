36b42cfed42201e32e32506c6d72ef68362171af
apxllo1 <bravounlimitaura@gmail.com>
2026-04-27 10:18:59 -0500

    Update store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,10 +2,14 @@ import Rodux from "@rbxts/rodux";
 import { dashboardReducer } from "store/reducers/dashboard.reducer";
 import { jobsReducer } from "store/reducers/jobs.reducer";
 import { optionsReducer } from "store/reducers/options.reducer";
+import { JobsAction } from "store/actions/jobs.action"; // Import your action type
+
+// Combine all possible action types in the store
+export type RootAction = JobsAction | Rodux.Action<string> | any; 
 
 export type RootReducer = typeof rootReducer;
-export type RootStore = Rodux.Store<RootState, Rodux.Action>;
 export type RootState = ReturnType<RootReducer>;
+export type RootStore = Rodux.Store<RootState, RootAction>;
 
 const rootReducer = Rodux.combineReducers({
 	dashboard: dashboardReducer,
@@ -14,5 +18,6 @@ const rootReducer = Rodux.combineReducers({
 });
 
 export function configureStore(initialState?: Partial<RootState>) {
-	return new Rodux.Store(rootReducer, initialState);
+	// We cast to RootStore to ensure the store accepts our custom Actions
+	return new Rodux.Store(rootReducer, initialState) as RootStore;
 }
