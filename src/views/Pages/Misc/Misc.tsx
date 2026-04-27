2d1c4cdd471fffe213166d2c65ac958c130c4fe2
apxllo1 <bravounlimitaura@gmail.com>
2026-04-26 23:01:11 -0500

    Replace button with scrolling frame for settings

--- src/views/Pages/Misc/Misc.tsx
+++ src/views/Pages/Misc/Misc.tsx
@@ -10,56 +10,22 @@ function MiscPage() {
 	return (
 		<frame Size={new UDim2(1, 0, 1, 0)} BackgroundTransparency={1}>
 			<uipadding PaddingTop={new UDim(0, 20)} PaddingLeft={new UDim(0, 20)} PaddingRight={new UDim(0, 20)} />
-			
-			<textbutton
-				Text="Open Facebang Settings"
-				Size={new UDim2(1, 0, 0, 50)}
-				BackgroundColor3={theme.button.background}
-				TextColor3={theme.button.foreground}
-				Font={Enum.Font.GothamBold}
-				TextSize={16}
-				Event={{ Activated: () => setModalVisible(true) }}
-			>
-				<uicorner CornerRadius={new UDim(0, 8)} />
-			</textbutton>
+			<scrollingframe Size={new UDim2(1, 0, 1, 0)} BackgroundTransparency={1} ScrollBarThickness={2}>
+				<uilistlayout Padding={new UDim(0, 10)} SortOrder="LayoutOrder" />
+				<textbutton
+					Text="Facebang Settings"
+					Size={new UDim2(1, 0, 0, 50)}
+					BackgroundColor3={theme.button.background}
+					TextColor3={theme.button.foreground}
+					Font={Enum.Font.GothamBold}
+					TextSize={16}
+					Event={{ Activated: () => setModalVisible(true) }}
+				>
+					<uicorner CornerRadius={new UDim(0, 8)} />
+				</textbutton>
+			</scrollingframe>
 
-			<FacebangModal 
-				isVisible={modalVisible} 
-				onClose={() => setModalVisible(false)} 
-			/>
-		</frame>
-	);
-}
-
-export default hooked(MiscPage);import Roact from "@rbxts/roact";
-import { hooked, useState } from "@rbxts/roact-hooked";
-import { useTheme } from "hooks/use-theme";
-import FacebangModal from "./FacebangModal";
-
-function MiscPage() {
-	const theme = useTheme("home").profile;
-	const [modalVisible, setModalVisible] = useState(false);
-
-	return (
-		<frame Size={new UDim2(1, 0, 1, 0)} BackgroundTransparency={1}>
-			<uipadding PaddingTop={new UDim(0, 20)} PaddingLeft={new UDim(0, 20)} PaddingRight={new UDim(0, 20)} />
-			
-			<textbutton
-				Text="Open Facebang Settings"
-				Size={new UDim2(1, 0, 0, 50)}
-				BackgroundColor3={theme.button.background}
-				TextColor3={theme.button.foreground}
-				Font={Enum.Font.GothamBold}
-				TextSize={16}
-				Event={{ Activated: () => setModalVisible(true) }}
-			>
-				<uicorner CornerRadius={new UDim(0, 8)} />
-			</textbutton>
-
-			<FacebangModal 
-				isVisible={modalVisible} 
-				onClose={() => setModalVisible(false)} 
-			/>
+			<FacebangModal isVisible={modalVisible} onClose={() => setModalVisible(false)} />
 		</frame>
 	);
 }
