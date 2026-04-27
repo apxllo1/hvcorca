2688e58c8dab7424df4b07e0a2826f2a4cd4e93b
apxllo1 <bravounlimitaura@gmail.com>
2026-04-27 10:27:58 -0500

    Update jobs.reducer.ts

--- src/store/reducers/jobs.reducer.ts
+++ src/store/reducers/jobs.reducer.ts
@@ -1,4 +1,23 @@
-// ... (initialState stays the same)
+import Rodux from "@rbxts/rodux";
+import { JobsAction } from "store/actions/jobs.action";
+import { JobsState, JobWithValue, JobWithSliders } from "../models/jobs.model";
+
+const initialState: JobsState = {
+	flight: { value: 60, active: false },
+	walkSpeed: { value: 80, active: false },
+	jumpHeight: { value: 200, active: false },
+	refresh: { active: false },
+	ghost: { active: false },
+	godmode: { active: false },
+	freecam: { active: false },
+	teleport: { active: false },
+	hide: { active: false },
+	kill: { active: false },
+	spectate: { active: false },
+	facebang: { active: false, sliders: { angle: 180, distance: 2.5 } },
+	rejoinServer: { active: false },
+	switchServer: { active: false },
+};
 
 export const jobsReducer = Rodux.createReducer<JobsState, JobsAction>(initialState, {
 	"jobs/setJobActive": (state, action) => ({
