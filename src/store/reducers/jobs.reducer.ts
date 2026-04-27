6775dae2622596e4b2efdbe8dd7ba0094745a70b
apxllo1 <bravounlimitaura@gmail.com>
2026-04-27 10:24:01 -0500

    Update jobs.reducer.ts

--- src/store/reducers/jobs.reducer.ts
+++ src/store/reducers/jobs.reducer.ts
@@ -1,34 +1,15 @@
-import Rodux from "@rbxts/rodux";
-import { JobsAction } from "store/actions/jobs.action";
-import { JobsState } from "../models/jobs.model";
-
-const initialState: JobsState = {
-	flight: { value: 60, active: false },
-	walkSpeed: { value: 80, active: false },
-	jumpHeight: { value: 200, active: false },
-	refresh: { active: false },
-	ghost: { active: false },
-	godmode: { active: false },
-	freecam: { active: false },
-	teleport: { active: false },
-	hide: { active: false },
-	kill: { active: false },
-	spectate: { active: false },
-	facebang: { active: false, sliders: { angle: 180, distance: 2.5 } },
-	rejoinServer: { active: false },
-	switchServer: { active: false },
-};
+// ... (initialState stays the same)
 
 export const jobsReducer = Rodux.createReducer<JobsState, JobsAction>(initialState, {
-	setJobActive: (state, action) => ({
+	"jobs/setJobActive": (state, action) => ({
 		...state,
 		[action.jobName]: { ...state[action.jobName], active: action.active },
 	}),
-	setJobValue: (state, action) => ({
+	"jobs/setJobValue": (state, action) => ({
 		...state,
 		[action.jobName]: { ...state[action.jobName], value: action.value },
 	}),
-	setJobSlider: (state, action) => {
+	"jobs/setJobSlider": (state, action) => {
 		const job = state[action.jobName];
 		if ("sliders" in job) {
 			return {
