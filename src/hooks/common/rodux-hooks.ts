4ac8bb95d7aa7c360dd4c670cafa121e604ded00
apxllo1 <bravounlimitaura@gmail.com>
2026-04-26 23:12:26 -0500

    Update rodux-hooks.ts

--- src/hooks/common/rodux-hooks.ts
+++ src/hooks/common/rodux-hooks.ts
@@ -1,18 +1,20 @@
 import { TypedUseSelectorHook, useDispatch as useBaseDispatch, useSelector as useBaseSelector, useStore as useBaseStore } from "@rbxts/roact-rodux-hooked";
 import { RootState } from "store/store";
+import { Store } from "@rbxts/rodux";
 
-// We define a simple Action interface here so we don't need the external 'rodux' library
 interface Action {
 	type: string;
 }
 
-// Export the standard names
+// 1. Standard Hooks
 export const useSelector: TypedUseSelectorHook<RootState> = useBaseSelector;
 export const useDispatch: () => (action: Action) => void = useBaseDispatch;
-// Fix: Use 'ReturnType' to get the proper type of the store
-export const useStore = (): ReturnType<typeof useBaseStore> => useBaseStore();
 
-// Export the "App" names that the rest of your project uses
+// 2. The Store Hook - This is the part causing the Shortcuts.tsx error
+// We explicitly tell it that the store uses our RootState
+export const useStore: () => Store<RootState> = useBaseStore;
+
+// 3. The "App" Hooks for your project
 export const useAppSelector = useSelector;
 export const useAppDispatch = useDispatch;
 export const useAppStore = useStore;
