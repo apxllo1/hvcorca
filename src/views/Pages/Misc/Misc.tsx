62f6258c24f7ce9965d7921501ded91ed80ebbd8
apxllo1 <bravounlimitaura@gmail.com>
2026-04-26 20:19:25 -0500

    Replace frame with scrollingframe in MiscPage

--- src/views/Pages/Misc/Misc.tsx
+++ src/views/Pages/Misc/Misc.tsx
@@ -7,20 +7,24 @@ function MiscPage() {
 	const theme = useTheme("home").profile;
 
 	return (
-		<frame
+		<scrollingframe
 			Size={new UDim2(1, -40, 1, -40)}
 			Position={new UDim2(0, 20, 0, 20)}
 			BackgroundTransparency={1}
+			BorderSizePixel={0}
+			ScrollBarThickness={2}
 		>
+			<uilistlayout Padding={new UDim(0, 10)} SortOrder="LayoutOrder" />
+			
 			<ActionButton 
 				action="facebang" 
 				theme={theme} 
-				hint="Teleport to and 'facebang' the selected target"
-				image="rbxassetid://10734950309" // A decent default icon
+				hint="Facebang the selected player"
+				image="rbxassetid://10734950309"
 				position={new UDim2(0, 0, 0, 0)}
 				canDeactivate 
 			/>
-		</frame>
+		</scrollingframe>
 	);
 }
 
