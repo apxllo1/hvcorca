bcf0925fe89c4d04b7691fb1a93510126b70bb99
apxllo1 <bravounlimitaura@gmail.com>
2026-04-27 10:23:32 -0500

    Update jobs.action.ts

--- src/store/actions/jobs.action.ts
+++ src/store/actions/jobs.action.ts
@@ -1,24 +1,24 @@
 import { JobsState, InferJobValue } from "store/models/jobs.model";
 
 export type JobsAction = 
-	| { type: "setJobActive"; jobName: keyof JobsState; active: boolean }
-	| { type: "setJobValue"; jobName: keyof JobsState; value: unknown }
-	| { type: "setJobSlider"; jobName: keyof JobsState; slider: string; value: number };
+	| { type: "jobs/setJobActive"; jobName: keyof JobsState; active: boolean }
+	| { type: "jobs/setJobValue"; jobName: keyof JobsState; value: unknown }
+	| { type: "jobs/setJobSlider"; jobName: keyof JobsState; slider: string; value: number };
 
 export const setJobActive = (jobName: keyof JobsState, active: boolean): JobsAction => ({
-	type: "setJobActive",
+	type: "jobs/setJobActive",
 	jobName,
 	active,
 });
 
-export const setJobValue = <K extends keyof JobsState>(jobName: K, value: InferJobValue<JobsState[K]>): JobsAction => ({
-	type: "setJobValue",
+export const setJobValue = (jobName: keyof JobsState, value: unknown): JobsAction => ({
+	type: "jobs/setJobValue",
 	jobName,
 	value,
 });
 
 export const setJobSlider = (jobName: keyof JobsState, slider: string, value: number): JobsAction => ({
-	type: "setJobSlider",
+	type: "jobs/setJobSlider",
 	jobName,
 	slider,
 	value,
