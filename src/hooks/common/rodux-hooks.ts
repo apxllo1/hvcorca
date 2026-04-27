298dfe8e4af031add57ed260f16f3f3e6922aa28
apxllo1 <bravounlimitaura@gmail.com>
2026-04-26 23:06:19 -0500

    Refactor hooks to include typed useStore and useApp hooks

--- src/hooks/common/rodux-hooks.ts
+++ src/hooks/common/rodux-hooks.ts
@@ -1,7 +1,13 @@
-import { TypedUseSelectorHook, useDispatch as useBaseDispatch, useSelector as useBaseSelector, useStore } from "@rbxts/roact-rodux-hooked";
+import { TypedUseSelectorHook, useDispatch as useBaseDispatch, useSelector as useBaseSelector, useStore as useBaseStore } from "@rbxts/roact-rodux-hooked";
 import { RootState } from "store/store";
+import { AnyAction } from "rodux";
 
-// We must explicitly export these so other files can use them!
+// Export the standard names
 export const useSelector: TypedUseSelectorHook<RootState> = useBaseSelector;
-export const useDispatch = useBaseDispatch;
-export { useStore };
+export const useDispatch: () => (action: AnyAction) => void = useBaseDispatch;
+export const useStore: () => useBaseStore<RootState> = useBaseStore;
+
+// Export the "App" names that your other files are looking for
+export const useAppSelector = useSelector;
+export const useAppDispatch = useDispatch;
+export const useAppStore = useStore;
