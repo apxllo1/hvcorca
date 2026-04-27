6e57d4b88177907b777673b25a2641737ff834de
apxllo1 <bravounlimitaura@gmail.com>
2026-04-26 23:10:08 -0500

    Refactor useDispatch and useStore with custom Action type

--- src/hooks/common/rodux-hooks.ts
+++ src/hooks/common/rodux-hooks.ts
@@ -1,13 +1,18 @@
 import { TypedUseSelectorHook, useDispatch as useBaseDispatch, useSelector as useBaseSelector, useStore as useBaseStore } from "@rbxts/roact-rodux-hooked";
 import { RootState } from "store/store";
-import { AnyAction } from "rodux";
+
+// We define a simple Action interface here so we don't need the external 'rodux' library
+interface Action {
+	type: string;
+}
 
 // Export the standard names
 export const useSelector: TypedUseSelectorHook<RootState> = useBaseSelector;
-export const useDispatch: () => (action: AnyAction) => void = useBaseDispatch;
-export const useStore: () => useBaseStore<RootState> = useBaseStore;
+export const useDispatch: () => (action: Action) => void = useBaseDispatch;
+// Fix: Use 'ReturnType' to get the proper type of the store
+export const useStore = (): ReturnType<typeof useBaseStore> => useBaseStore();
 
-// Export the "App" names that your other files are looking for
+// Export the "App" names that the rest of your project uses
 export const useAppSelector = useSelector;
 export const useAppDispatch = useDispatch;
 export const useAppStore = useStore;
