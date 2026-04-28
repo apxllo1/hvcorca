bfd084271aade2711a3d7ef9c3dbaab96200bf6e
apxllo1 <bravounlimitaura@gmail.com>
2026-04-27 22:31:00 -0500

    Update global.d.ts

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,12 +1,27 @@
+/**
+ * Global variables injected by the Havoc Bundler & Runtime
+ */
 declare const VERSION: string;
 
+/**
+ * Roblox Executor Globals
+ */
 declare const queue_on_teleport: ((script: string) => void) | undefined;
-
 declare const gethui: (() => BasePlayerGui) | undefined;
-
 declare const protect_gui: ((object: ScreenGui) => void) | undefined;
+declare function getgenv(): any;
 
 declare namespace syn {
 	function queue_on_teleport(script: string): void;
 	function protect_gui(object: ScreenGui): void;
+}
+
+/**
+ * Havoc Runtime Handshake Globals
+ * (Optional: only needed if you call these from TS)
+ */
+declare namespace _G {
+    let Havoc_Init: (env: any) => void;
+    let Havoc_NewModule: (...args: any[]) => void;
+    let Havoc_NewInstance: (...args: any[]) => void;
 }
