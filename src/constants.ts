3eca967ee61fafe156edc4a52ce8a1ed1ed9df34
apxllo1 <bravounlimitaura@gmail.com>
2026-04-29 21:18:17 -0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,7 @@
-export const IS_DEV = getgenv === undefined;
+declare const VERSION: string | undefined;
+
+// Use type check for getgenv to prevent "cannot find name" errors during compile
+export const IS_DEV = type(getgenv) === "nil";
+
+// Fallback to "studio" if VERSION is not injected during bundling
 export const VERSION_TAG = VERSION ?? "studio";
