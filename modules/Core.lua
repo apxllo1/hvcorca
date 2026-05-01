e125e00875fca1c37a984d617df29a37c57fe02a
apxllo1 <bravounlimitaura@gmail.com>
2026-05-01 18:28:32 -0500

    Create Core.lua

---
+++ modules/Core.lua
@@ -0,0 +1,12 @@
+--[[
+    Core.lua – HAVOC v2.0 Core Module
+]]--
+
+local Core = {}
+
+function Core.init()
+    print("🧠 HAVOC Core initialized")
+    -- later you can add real logic here
+end
+
+return Core
