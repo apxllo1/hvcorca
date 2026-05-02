59085ac157ee034ac923a4c826d72cf0fc3d0680
apxllo1 <bravounlimitaura@gmail.com>
2026-05-01 22:43:48 -0500

    Initial commit

---
+++ src/store/actions/misc.action.ts
@@ -0,0 +1,22 @@
+import { Rodux } from "@rbxts/rodux";
+import { GistLoaderState } from "../models/misc.model";
+
+export type MiscAction =
+	| Rodux.InferActionFromCreator<typeof setGistActive>
+	| Rodux.InferActionFromCreator<typeof setCurrentGist>
+	| Rodux.InferActionFromCreator<typeof clearGistError>;
+
+export const setGistActive = Rodux.makeActionCreator(
+	"misc/setGistActive",
+	(active: boolean) => ({ active }),
+);
+
+export const setCurrentGist = Rodux.makeActionCreator(
+	"misc/setCurrentGist",
+	(gist: { url: string; id: string } | null) => ({ gist }),
+);
+
+export const clearGistError = Rodux.makeActionCreator(
+	"misc/clearGistError",
+	() => ({}),
+);
