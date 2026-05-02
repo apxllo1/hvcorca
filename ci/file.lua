4926e0bcf7ea430c0a2c1cc80346a6153d32604c
apxllo1 <bravounlimitaura@gmail.com>
2026-05-01 20:30:46 -0500

    Update file.lua

--- ci/file.lua
+++ ci/file.lua
@@ -1,13 +1,6 @@
---[[
-    file.lua – HAVOC v2.0 FileWriter
-]]--
-
 local File = {}
-
 local OUTPUT_PATH = "public/latest.lua"
-
 function File.write(content)
     remodel.writeFile(OUTPUT_PATH, content)
 end
-
 return File
