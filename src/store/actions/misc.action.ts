528fa061e00c4018ec983d9caf577b60df3792ac
apxllo1 <bravounlimitaura@gmail.com>
2026-05-02 11:48:17 -0500

    Update misc.action.ts

--- src/store/actions/misc.action.ts
+++ src/store/actions/misc.action.ts
@@ -6,11 +6,19 @@ export type MiscAction =
 	| Rodux.InferActionFromCreator<typeof setCurrentGist>
 	| Rodux.InferActionFromCreator<typeof clearGistError>;
 
-export const setGistActive = Rodux.makeActionCreator("misc/setGistActive", (active: boolean) => ({ active }));
+export const setGistActive = Rodux.makeActionCreator(
+	"misc/setGistActive",
+	(active: boolean) => ({ active }),
+);
 
 export const setCurrentGist = Rodux.makeActionCreator(
 	"misc/setCurrentGist",
-	(gist: { url: string; id: string } | undefined) => ({ gist }),
+	(gist: { url: string; id: string } | undefined) => {
+		return { gist: gist ?? null };
+	},
 );
 
-export const clearGistError = Rodux.makeActionCreator("misc/clearGistError", () => ({}));
+export const clearGistError = Rodux.makeActionCreator(
+	"misc/clearGistError",
+	() => ({}),
+);
