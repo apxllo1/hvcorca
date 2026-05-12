113b1ec033720e698f2ddddca5f37828b0f90d46
apxllo1 <bravounlimitaura@gmail.com>
2026-05-12 17:21:23 -0500

    Refactor teleport job logic and cleanup imports

--- src/jobs/players/teleport.ts
+++ src/jobs/players/teleport.ts
@@ -2,30 +2,26 @@ import { Players } from "@rbxts/services";
 import { getSelectedPlayer } from "jobs/helpers/get-selected-player";
 import { getStore, onJobChange } from "jobs/helpers/job-store";
 import { setJobActive } from "store/actions/jobs.action";
-import { setTimeout, Timeout } from "utils/timeout";
+import { setTimeout } from "utils/timeout";
+import type { Timeout } from "utils/timeout";
 
 async function main() {
 	const store = await getStore();
 	const playerSelected = await getSelectedPlayer(() => {
 		store.dispatch(setJobActive("teleport", false));
 	});
-
 	let timeout: Timeout | undefined;
-
-	await onJobChange("teleport", (job) => {
+	onJobChange("teleport", (job) => {
 		timeout?.clear();
 		timeout = undefined;
-
 		if (job.active) {
 			const rootPart = Players.LocalPlayer.Character?.FindFirstChild("HumanoidRootPart");
 			const targetRootPart = playerSelected.current?.Character?.FindFirstChild("HumanoidRootPart");
-
-			if (!targetRootPart || !rootPart || !rootPart.IsA("BasePart") || !targetRootPart.IsA("BasePart")) {
+			if (!rootPart || !targetRootPart || !rootPart.IsA("BasePart") || !targetRootPart.IsA("BasePart")) {
 				store.dispatch(setJobActive("teleport", false));
 				warn(`[teleport-worker] Failed to find root parts (${rootPart} -> ${targetRootPart})`);
 				return;
 			}
-
 			timeout = setTimeout(() => {
 				store.dispatch(setJobActive("teleport", false));
 				rootPart.CFrame = targetRootPart.CFrame.mul(new CFrame(0, 0, 1));
