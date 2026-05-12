9d9cc8d53ed1f8ce56f6d6f7917260d9abdbeff4
apxllo1 <bravounlimitaura@gmail.com>
2026-05-12 17:13:11 -0500

    Update godmode.ts

--- src/jobs/character/godmode.ts
+++ src/jobs/character/godmode.ts
@@ -1,9 +1,8 @@
 import { Players, Workspace } from "@rbxts/services";
 import { getStore, onJobChange } from "jobs/helpers/job-store";
-import { JobsAction } from "store/actions/jobs.action";
+import type { JobsAction } from "store/actions/jobs.action";
 
 const player = Players.LocalPlayer;
-
 let currentCharacter: Model | undefined;
 
 async function main() {
@@ -11,8 +10,7 @@ async function main() {
 		warn(`[godmode-worker] ${err}`);
 		deactivate();
 	}
-
-	await onJobChange("godmode", (job, state) => {
+	onJobChange("godmode", (job, state) => {
 		if (state.jobs.ghost.active && job.active) {
 			// Can't enable godmode while ghost
 			deactivate();
@@ -44,23 +42,18 @@ async function deactivateOnCharacterAdded() {
 // https://github.com/EdgeIY/infiniteyield/blob/master/source#L9043
 async function activateGodmode() {
 	const cameraCFrame = Workspace.CurrentCamera!.CFrame;
-
 	const character = player.Character;
 	if (!character) {
 		throw "Character is null";
 	}
-
 	const humanoid = character.FindFirstChildWhichIsA("Humanoid");
 	if (!humanoid) {
 		throw "No humanoid found";
 	}
-
 	const mockHumanoid = humanoid.Clone();
 	mockHumanoid.Parent = character;
 	currentCharacter = character;
-
 	player.Character = undefined;
-
 	// TODO: Check if these are optional
 	mockHumanoid.SetStateEnabled(Enum.HumanoidStateType.Dead, false);
 	mockHumanoid.SetStateEnabled(Enum.HumanoidStateType.Ragdoll, false);
@@ -68,21 +61,18 @@ async function activateGodmode() {
 	mockHumanoid.BreakJointsOnDeath = true;
 	mockHumanoid.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None;
 	humanoid.Destroy();
-
 	player.Character = character;
 	Workspace.CurrentCamera!.CameraSubject = mockHumanoid;
 	task.defer(() => {
 		// TODO: Check if this is optional
 		Workspace.CurrentCamera!.CFrame = cameraCFrame;
 	});
-
 	const animation = character.FindFirstChild("Animate") as LocalScript | undefined;
 	if (animation) {
 		// Re-run the animation script for the new Humanoid
 		animation.Disabled = true;
 		animation.Disabled = false;
 	}
-
 	// Mark the character as godmode
 	mockHumanoid.MaxHealth = math.huge;
 	mockHumanoid.Health = mockHumanoid.MaxHealth;
