07be496eee748f7c337f79e9094b0f862ea2df30
apxllo1 <bravounlimitaura@gmail.com>
2026-05-12 17:07:06 -0500

    Change import to use type for DashboardPage

--- src/hooks/use-current-page.ts
+++ src/hooks/use-current-page.ts
@@ -1,5 +1,5 @@
 import { useAppSelector } from "hooks/common/rodux-hooks";
-import { DashboardPage } from "store/models/dashboard.model";
+import type { DashboardPage } from "store/models/dashboard.model";
 
 export function useCurrentPage() {
 	return useAppSelector((state) => state.dashboard.page);
