2f1d6e810b8a403fc303ebbfde84d77eb9280555
apxllo1 <bravounlimitaura@gmail.com>
2026-05-12 17:14:22 -0500

    Refactor job change handling in humanoid.ts

--- src/jobs/character/humanoid.ts
+++ src/jobs/character/humanoid.ts
@@ -1,9 +1,8 @@
 import { Players } from "@rbxts/services";
 import { getStore, onJobChange } from "jobs/helpers/job-store";
-import { JobWithValue } from "store/models/jobs.model";
+import type { JobWithValue } from "store/models/jobs.model";
 
 const JUMP_POWER_CONSTANT = 349.24;
-
 const player = Players.LocalPlayer;
 const defaults = {
 	walkSpeed: 16,
@@ -12,38 +11,31 @@ const defaults = {
 
 async function main() {
 	const store = await getStore();
-
 	let humanoid = player.Character?.FindFirstChildWhichIsA("Humanoid");
-
 	// References to the Humanoid state in the Rodux store.
 	const state = store.getState();
 	let walkSpeedJob = state.jobs.walkSpeed;
 	let jumpHeightJob = state.jobs.jumpHeight;
-
-	await onJobChange("walkSpeed", (job) => {
+	onJobChange("walkSpeed", (job) => {
 		if (job.active && !walkSpeedJob.active) {
 			setDefaultWalkSpeed(humanoid);
 		}
 		walkSpeedJob = job;
 		updateWalkSpeed(humanoid, walkSpeedJob);
 	});
-
-	await onJobChange("jumpHeight", (job) => {
+	onJobChange("jumpHeight", (job) => {
 		if (job.active && !jumpHeightJob.active) {
 			setDefaultJumpHeight(humanoid);
 		}
 		jumpHeightJob = job;
 		updateJumpHeight(humanoid, jumpHeightJob);
 	});
-
 	player.CharacterAdded.Connect((character) => {
 		const newHumanoid = character.WaitForChild("Humanoid", 5);
 		if (newHumanoid && newHumanoid.IsA("Humanoid")) {
 			humanoid = newHumanoid;
-
 			setDefaultWalkSpeed(newHumanoid);
 			setDefaultJumpHeight(newHumanoid);
-
 			if (walkSpeedJob.active) {
 				updateWalkSpeed(newHumanoid, walkSpeedJob);
 			}
@@ -52,7 +44,6 @@ async function main() {
 			}
 		}
 	});
-
 	setDefaultWalkSpeed(humanoid);
 	setDefaultJumpHeight(humanoid);
 }
