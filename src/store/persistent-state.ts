50e76c8c0c8f2ac964e225882b11793ae87410bd
apxllo1 <bravounlimitaura@gmail.com>
2026-05-12 17:29:03 -0500

    Change import to use type for RootState

--- src/store/persistent-state.ts
+++ src/store/persistent-state.ts
@@ -1,6 +1,6 @@
 import { HttpService, Players } from "@rbxts/services";
 import { getStore } from "../jobs/helpers/job-store";
-import { RootState } from "./store";
+import type { RootState } from "./store";
 import { setInterval } from "../utils/timeout";
 
 if (typeIs(makefolder, "function") && !isfolder("_orca")) {
@@ -23,13 +23,11 @@ function write(file: string, content: string): void {
 export function persistentState<T extends object>(name: string, selector: (state: RootState) => T, defaultValue: T): T {
 	try {
 		const serializedState = read(`_orca/${name}.json`);
-
 		if (serializedState === undefined) {
 			write(`_orca/${name}.json`, HttpService.JSONEncode(defaultValue));
 			autosave(name, selector);
 			return defaultValue;
 		}
-
 		const value = HttpService.JSONDecode(serializedState) as T;
 		autosave(name, selector);
 		return value;
@@ -41,7 +39,6 @@ export function persistentState<T extends object>(name: string, selector: (state
 
 async function autosave(name: string, selector: (state: RootState) => object) {
 	const store = await getStore();
-
 	const save = () => {
 		try {
 			const state = selector(store.getState());
@@ -50,9 +47,7 @@ async function autosave(name: string, selector: (state: RootState) => object) {
 			warn(`[PersistentState] Autosave failed for ${name}: ${err}`);
 		}
 	};
-
 	setInterval(save, 60000);
-
 	Players.PlayerRemoving.Connect((player) => {
 		if (player === Players.LocalPlayer) {
 			save();
