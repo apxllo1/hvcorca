ffb698d3bc2c1442bfcfa41fe3ecf7d20c0918c6
apxllo1 <bravounlimitaura@gmail.com>
2026-05-12 17:08:32 -0500

    Change import to use type for Theme interface

--- src/hooks/use-theme.ts
+++ src/hooks/use-theme.ts
@@ -1,7 +1,7 @@
 import { useAppSelector } from "hooks/common/rodux-hooks";
 import { getThemes } from "themes";
 import { darkTheme } from "themes/sorbet";
-import { Theme } from "themes/theme.interface";
+import type { Theme } from "themes/theme.interface";
 
 const THEME_MAP = new Map(getThemes().map((t) => [t.name, t]));
 
@@ -9,7 +9,6 @@ export function useTheme<K extends keyof Theme>(key: K): Theme[K] {
 	return useAppSelector((state) => {
 		const themeName = state.options.currentTheme;
 		const theme = THEME_MAP.get(themeName) ?? darkTheme;
-
 		return theme[key];
 	});
 }
