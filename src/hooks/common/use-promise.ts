84f5e3e4ca79bf5e3ebc4766876f93e0d28ae9c7
apxllo1 <bravounlimitaura@gmail.com>
2026-05-12 17:44:27 -0500

    Refactor usePromise hook for better error handling
    Updated the usePromise hook to handle error types more accurately and prevent potential issues with undefined error states.

--- src/hooks/common/use-promise.ts
+++ src/hooks/common/use-promise.ts
@@ -1,33 +1,40 @@
 import { useEffect, useReducer } from "@rbxts/roact-hooked";
 import type { Reducer } from "@rbxts/roact-hooked";
+
 // https://github.com/bsonntag/react-use-promise
 type PromiseState = "pending" | "rejected" | "resolved";
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 interface State<T = any> {
 	result?: T;
 	err?: unknown;
 	state: PromiseState;
 }
+
 interface Action {
 	type: PromiseState;
 	payload?: unknown;
 }
+
 function resolvePromise<T>(promise: Promise<T> | (() => Promise<T>)): Promise<T> {
 	if (typeIs(promise, "function")) {
 		return promise();
 	}
 	return promise;
 }
+
 const states: Record<PromiseState, PromiseState> = {
 	pending: "pending",
 	rejected: "rejected",
 	resolved: "resolved",
 };
+
 const defaultState: State = {
 	err: undefined,
 	result: undefined,
 	state: states.pending,
 };
+
 function reducer<T>(state: State<T>, action: Action): State<T> {
 	switch (action.type) {
 		case states.pending:
@@ -49,10 +56,11 @@ function reducer<T>(state: State<T>, action: Action): State<T> {
 			return state;
 	}
 }
+
 export function usePromise<T>(
 	promise: Promise<T> | (() => Promise<T>),
 	deps: unknown[] = [],
-): [result: T | undefined, err: unknown | undefined, state: PromiseState] {
+): [result: T | undefined, err: unknown, state: PromiseState] {
 	const [{ err, result, state }, dispatch] = useReducer(reducer as Reducer<State<T>, Action>, defaultState);
 	useEffect(() => {
 		promise = resolvePromise<T>(promise);
@@ -61,7 +69,7 @@ export function usePromise<T>(
 		}
 		let canceled = false;
 		dispatch({ type: states.pending });
-		promise.then(
+		void promise.then(
 			(result) => {
 				if (!canceled) {
 					dispatch({
