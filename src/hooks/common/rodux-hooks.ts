7264aa86f96b4b4e3c1966bf11074030608a7934
apxllo1 <bravounlimitaura@gmail.com>
2026-05-12 10:03:03 -0500

    Refactor imports in rodux-hooks.ts
    Refactor imports to use type imports for better clarity.

--- src/hooks/common/rodux-hooks.ts
+++ src/hooks/common/rodux-hooks.ts
@@ -1,10 +1,8 @@
-import { TypedUseSelectorHook, useDispatch, useSelector, useStore } from "@rbxts/roact-rodux-hooked";
-import Rodux from "@rbxts/rodux";
-
-import { RootState, RootStore } from "store/store";
-
+import type { TypedUseSelectorHook } from "@rbxts/roact-rodux-hooked";
+import { useDispatch, useSelector, useStore } from "@rbxts/roact-rodux-hooked";
+import type Rodux from "@rbxts/rodux";
+import type { RootState, RootStore } from "store/store";
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppDispatch = () => useDispatch<Rodux.Action>();
 export const useAppStore = () => useStore<RootStore>();
-
 export { useSelector, useDispatch };
