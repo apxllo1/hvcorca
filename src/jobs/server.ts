be4bc750033eab153cba8043e8ce64568ddd0ba0
apxllo1 <bravounlimitaura@gmail.com>
2026-05-12 17:23:30 -0500

    Refactor timeout import and clean up code

--- src/jobs/server.ts
+++ src/jobs/server.ts
@@ -2,7 +2,8 @@ import { HttpService, Players, TeleportService } from "@rbxts/services";
 import { getStore, onJobChange } from "jobs/helpers/job-store";
 import { setJobActive } from "store/actions/jobs.action";
 import * as http from "utils/http";
-import { setTimeout, Timeout } from "utils/timeout";
+import { setTimeout } from "utils/timeout";
+import type { Timeout } from "utils/timeout";
 
 interface GameServer {
 	id: string;
@@ -16,16 +17,13 @@ interface GameServersResponse {
 
 async function onServerHop(): Promise<void> {
 	queueExecution();
-
 	const serversResult = await http.get(
 		`https://games.roblox.com/v1/games/${game.PlaceId}/servers/Public?sortOrder=Asc&limit=100`,
 	);
 	const servers = HttpService.JSONDecode(serversResult) as GameServersResponse;
-
 	const serversAvailable = servers.data.filter(
 		(server) => server.playing < server.maxPlayers && server.id !== game.JobId,
 	);
-
 	if (serversAvailable.size() === 0) {
 		throw "[server-worker-switch] No servers available.";
 	} else {
@@ -36,7 +34,6 @@ async function onServerHop(): Promise<void> {
 
 async function onRejoin(): Promise<void> {
 	queueExecution();
-
 	if (Players.GetPlayers().size() === 1) {
 		TeleportService.Teleport(game.PlaceId, Players.LocalPlayer);
 	} else {
@@ -47,27 +44,21 @@ async function onRejoin(): Promise<void> {
 function queueExecution(): void {
 	const code =
 		'loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/apxllo1/hvcorca/master/public/latest.lua"))()';
-
 	(syn?.queue_on_teleport ?? queue_on_teleport)?.(code);
 }
 
 async function main(): Promise<void> {
 	const store = await getStore();
-
 	let timeout: Timeout | undefined;
-
 	function clearTimeout(): void {
 		timeout?.clear();
 		timeout = undefined;
 	}
-
-	await onJobChange("rejoinServer", (job, state) => {
+	onJobChange("rejoinServer", (job, state) => {
 		clearTimeout();
-
 		if (state.jobs.switchServer.active) {
 			store.dispatch(setJobActive("switchServer", false));
 		}
-
 		if (job.active) {
 			timeout = setTimeout(() => {
 				onRejoin().catch((err: unknown) => {
@@ -77,14 +68,11 @@ async function main(): Promise<void> {
 			}, 1000);
 		}
 	});
-
-	await onJobChange("switchServer", (job, state) => {
+	onJobChange("switchServer", (job, state) => {
 		clearTimeout();
-
 		if (state.jobs.rejoinServer.active) {
 			store.dispatch(setJobActive("rejoinServer", false));
 		}
-
 		if (job.active) {
 			timeout = setTimeout(() => {
 				onServerHop().catch((err: unknown) => {
