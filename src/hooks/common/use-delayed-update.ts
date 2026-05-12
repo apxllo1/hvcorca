27fe79c9ffc9b165da1d6335b398f4dd930fb79e
apxllo1 <bravounlimitaura@gmail.com>
2026-05-12 10:03:54 -0500

    Refactor useDelayedUpdate to improve clarity and structure

--- src/hooks/common/use-delayed-update.ts
+++ src/hooks/common/use-delayed-update.ts
@@ -1,13 +1,11 @@
 import { useEffect, useMutable, useState } from "@rbxts/roact-hooked";
-import { clearTimeout, setTimeout, Timeout } from "utils/timeout";
-
+import type { Timeout } from "utils/timeout";
+import { clearTimeout, setTimeout } from "utils/timeout";
 interface IncomingUpdate {
 	timeout: Timeout;
 	resolveTime: number;
 }
-
 let nextId = 0;
-
 function clearUpdates(updates: Map<number, IncomingUpdate>, laterThan?: number) {
 	for (const [id, update] of updates) {
 		if (laterThan === undefined || update.resolveTime >= laterThan) {
@@ -16,11 +14,9 @@ function clearUpdates(updates: Map<number, IncomingUpdate>, laterThan?: number) 
 		}
 	}
 }
-
 export function useDelayedUpdate<T>(value: T, delay: number, isImmediate?: (current: T) => boolean): T {
 	const [delayedValue, setDelayedValue] = useState(value);
 	const updates = useMutable(new Map<number, IncomingUpdate>());
-
 	useEffect(() => {
 		// Cancel all updates if the change is immediate
 		if (isImmediate?.(value)) {
@@ -28,7 +24,6 @@ export function useDelayedUpdate<T>(value: T, delay: number, isImmediate?: (curr
 			setDelayedValue(value);
 			return;
 		}
-
 		const id = nextId++;
 		const update: IncomingUpdate = {
 			timeout: setTimeout(() => {
@@ -37,16 +32,12 @@ export function useDelayedUpdate<T>(value: T, delay: number, isImmediate?: (curr
 			}, delay),
 			resolveTime: os.clock() + delay,
 		};
-
 		// Clear all updates that are later than the current one to prevent overlap
 		clearUpdates(updates.current, update.resolveTime);
-
 		updates.current.set(id, update);
 	}, [value]);
-
 	useEffect(() => {
 		return () => clearUpdates(updates.current);
 	}, []);
-
 	return delayedValue;
 }
