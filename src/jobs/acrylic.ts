93b89d6885a196b78d612f5a0e1de12be15589db
apxllo1 <bravounlimitaura@gmail.com>
2026-05-12 17:10:40 -0500

    Refactor acrylic.ts for better import and timeout usage
    Refactor imports and improve timeout handling in acrylic.ts.

--- src/jobs/acrylic.ts
+++ src/jobs/acrylic.ts
@@ -1,13 +1,15 @@
 import Make from "@rbxts/make";
 import { Lighting } from "@rbxts/services";
 import { getStore } from "jobs/helpers/job-store";
-import { setTimeout, Timeout } from "utils/timeout";
+import { setTimeout } from "utils/timeout";
+import type { Timeout } from "utils/timeout";
 
 const baseEffect = Make("DepthOfFieldEffect", {
 	FarIntensity: 0,
 	InFocusRadius: 0.1,
 	NearIntensity: 1,
 });
+
 const depthOfFieldDefaults = new Map<DepthOfFieldEffect, { enabled: boolean }>();
 
 function enableAcrylic() {
@@ -26,24 +28,19 @@ function disableAcrylic() {
 
 async function main() {
 	const store = await getStore();
-
 	for (const effect of Lighting.GetChildren()) {
 		if (effect.IsA("DepthOfFieldEffect")) {
 			depthOfFieldDefaults.set(effect, { enabled: effect.Enabled });
 		}
 	}
-
 	let timeout: Timeout | undefined;
-
 	store.changed.connect((newState) => {
 		timeout?.clear();
 		timeout = undefined;
-
 		if (!newState.dashboard.isOpen) {
 			timeout = setTimeout(disableAcrylic, 500);
 			return;
 		}
-
 		if (newState.options.config.acrylicBlur) {
 			enableAcrylic();
 		} else {
