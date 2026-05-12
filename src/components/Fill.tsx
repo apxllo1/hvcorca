98c1b0e10049d08ef4e3edfa3a7af723dec740c2
apxllo1 <bravounlimitaura@gmail.com>
2026-05-12 09:47:03 -0500

    Refactor imports to use type imports in Fill.tsx

--- src/components/Fill.tsx
+++ src/components/Fill.tsx
@@ -1,16 +1,15 @@
 import Roact from "@rbxts/roact";
-import { GradientTheme } from "themes/theme.interface";
-import { BindingOrValue, mapBinding } from "utils/binding-util";
+import type { GradientTheme } from "themes/theme.interface";
+import type { BindingOrValue } from "utils/binding-util";
+import { mapBinding } from "utils/binding-util";
 import { hex } from "utils/color3";
 import { scale } from "utils/udim2";
-
 interface Props extends Roact.PropsWithChildren {
 	color?: BindingOrValue<Color3>;
 	gradient?: GradientTheme;
 	transparency?: BindingOrValue<number>;
 	radius?: BindingOrValue<number | "circular">;
 }
-
 function Fill({ color = hex("#ffffff"), gradient, transparency = 0, radius = 0, [Roact.Children]: children }: Props) {
 	return (
 		<frame Size={scale(1, 1)} BackgroundColor3={color} BackgroundTransparency={transparency}>
@@ -22,17 +21,14 @@ function Fill({ color = hex("#ffffff"), gradient, transparency = 0, radius = 0, 
 					Rotation={gradient.rotation}
 				/>
 			)}
-
 			{radius !== undefined && (
 				<uicorner
 					Key="corner"
 					CornerRadius={mapBinding(radius, (r) => (r === "circular" ? new UDim(1, 0) : new UDim(0, r)))}
 				/>
 			)}
-
 			{children}
 		</frame>
 	);
 }
-
 export default Fill;
