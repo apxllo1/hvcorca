d7a0e0f902511322ca68e49e848ae127fe9a980b
apxllo1 <bravounlimitaura@gmail.com>
2026-05-13 21:32:32 -0500

    Refactor friendsJoined calculation for clarity

--- src/views/Pages/Home/Profile/Info.tsx
+++ src/views/Pages/Home/Profile/Info.tsx
@@ -1,38 +1,29 @@
 import Roact from "@rbxts/roact";
 import { Players } from "@rbxts/services";
-
 import Canvas from "components/Canvas";
-
 import { useDelayedUpdate } from "hooks/common/use-delayed-update";
 import { useSpring } from "hooks/common/use-spring";
 import { useIsPageOpen } from "hooks/use-current-page";
 import { useFriends } from "hooks/use-friends";
 import { useTheme } from "hooks/use-theme";
-
 import type { DashboardPage } from "store/models/dashboard.model";
 import { DashboardPage as DP } from "store/models/dashboard.model";
-
 import { px } from "utils/udim2";
 
 function Info() {
 	const theme = useTheme("home").profile;
 	const isOpen = useIsPageOpen(DP.Home);
-
 	const [friends = [], , status] = useFriends();
 
 	const friendsOnline = friends.size();
-	const friendsJoined = friends.filter(
-		(friend) => "PlaceId" in friend && friend.PlaceId === game.PlaceId,
-	).size();
+	const friendsJoined = friends.filter((friend) => "PlaceId" in friend && friend.PlaceId === game.PlaceId).size();
 
-	// Transition delays
 	const showJoinDate = useDelayedUpdate(isOpen, 400, (open) => !open);
 	const showFriendsJoined = useDelayedUpdate(isOpen && status !== "pending", 500, (open) => !open);
 	const showFriendsOnline = useDelayedUpdate(isOpen && status !== "pending", 600, (open) => !open);
 
 	return (
 		<Canvas anchor={new Vector2(0.5, 0)} size={px(278, 48)} position={new UDim2(0.5, 0, 0, 300)}>
-			{/* Dividers */}
 			<frame Size={px(0, 26)} Position={px(90, 11)} BackgroundTransparency={1}>
 				<uistroke Thickness={0.5} Color={theme.foreground} Transparency={0.7} />
 			</frame>
@@ -40,11 +31,12 @@ function Info() {
 				<uistroke Thickness={0.5} Color={theme.foreground} Transparency={0.7} />
 			</frame>
 
-			{/* Player info */}
 			<textlabel
 				Font="GothamBold"
 				Text={`Joined\n${
-					(os.date("%m/%d/%Y", os.time() - Players.LocalPlayer.AccountAge * 24 * 60 * 60) as string | undefined) ?? ""
+					(os.date("%m/%d/%Y", os.time() - Players.LocalPlayer.AccountAge * 24 * 60 * 60) as
+						| string
+						| undefined) ?? ""
 				}`}
 				TextSize={13}
 				TextColor3={theme.foreground}
@@ -55,7 +47,6 @@ function Info() {
 				Position={useSpring(showJoinDate ? px(0, 0) : px(-20, 0), {})}
 				BackgroundTransparency={1}
 			/>
-
 			<textlabel
 				Font="GothamBold"
 				Text={friendsJoined === 1 ? "1 friend\njoined" : `${friendsJoined} friends\njoined`}
@@ -68,7 +59,6 @@ function Info() {
 				Position={useSpring(showFriendsJoined ? px(97, 0) : px(97 - 20, 0), {})}
 				BackgroundTransparency={1}
 			/>
-
 			<textlabel
 				Font="GothamBold"
 				Text={friendsOnline === 1 ? "1 friend\nonline" : `${friendsOnline} friends\nonline`}
