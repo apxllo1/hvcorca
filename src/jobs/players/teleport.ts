6ee8f3f568604272f6c1a280b28c2dad87fe5229
apxllo1 <bravounlimitaura@gmail.com>
2026-05-14 22:24:36 -0500

    Improve error logging in teleport function

--- src/jobs/players/teleport.ts
+++ src/jobs/players/teleport.ts
@@ -10,26 +10,31 @@ async function main() {
 	const playerSelected = await getSelectedPlayer(() => {
 		store.dispatch(setJobActive("teleport", false));
 	});
+
 	let timeout: Timeout | undefined;
+
 	onJobChange("teleport", (job) => {
 		timeout?.clear();
 		timeout = undefined;
+
 		if (job.active) {
 			const rootPart = Players.LocalPlayer.Character?.FindFirstChild("HumanoidRootPart");
 			const targetRootPart = playerSelected.current?.Character?.FindFirstChild("HumanoidRootPart");
+
 			if (!rootPart || !targetRootPart || !rootPart.IsA("BasePart") || !targetRootPart.IsA("BasePart")) {
 				store.dispatch(setJobActive("teleport", false));
-				warn(`[teleport-worker] Failed to find root parts (${rootPart} -> ${targetRootPart})`);
+				warn(`[teleport-worker] Failed to find root parts (${String(rootPart)} -> ${String(targetRootPart)})`);
 				return;
 			}
+
 			timeout = setTimeout(() => {
 				store.dispatch(setJobActive("teleport", false));
 				rootPart.CFrame = targetRootPart.CFrame.mul(new CFrame(0, 0, 1));
 			}, 1000);
 		}
-	});
+	}).catch((err) => warn(`[teleport-worker] ${String(err)}`));
 }
 
 main().catch((err) => {
-	warn(`[teleport-worker] ${err}`);
+	warn(`[teleport-worker] ${String(err)}`);
 });
